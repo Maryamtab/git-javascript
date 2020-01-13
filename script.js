674b44bd61a2ec2c0a85f9ea43380f3f198eeee1
Maryam Taba <taba0027@algonquinlive.com>
2020-01-13 12:11:42 -0500

    Adds index.html and script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+let name = "Maryam Tabatabei";
+console.log(`Hello, my name is ${name}`);
+
+const number = 30
+result = number * 2
+console.log(result)
+console.log(result *= 5)
+console.log(result /= number)
+console.log(result -= 7)
+
